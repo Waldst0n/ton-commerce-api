ff4adc269f4ee7fb4ab0e1750a042f92de23eea3
Waldst0n <waldston.ws@gmail.com>
2026-02-17 11:35:57 -0300

    chore: initial commit

---
+++ src/test/java/com/waldstonsantana/toncommerce/ToncommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waldstonsantana.toncommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToncommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
