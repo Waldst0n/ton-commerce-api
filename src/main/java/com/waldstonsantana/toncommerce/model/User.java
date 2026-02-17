b7511b17841f78db1903b7c338f10051800ce07f
Waldst0n <waldston.ws@gmail.com>
2026-02-17 11:36:15 -0300

    feat: add User Model

---
+++ src/main/java/com/waldstonsantana/toncommerce/model/User.java
@@ -0,0 +1,36 @@
+package com.waldstonsantana.toncommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String phone;
+
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    private String password;
+
+}
