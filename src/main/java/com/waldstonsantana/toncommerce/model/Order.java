8c5dfb943a9d4f1a4ec73430e94f664a46809d6e
Waldst0n <waldston.ws@gmail.com>
2026-02-17 12:03:07 -0300

    feat: add OrderModel and orderStatus enum

---
+++ src/main/java/com/waldstonsantana/toncommerce/model/Order.java
@@ -0,0 +1,32 @@
+package com.waldstonsantana.toncommerce.model;
+
+
+import com.waldstonsantana.toncommerce.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private Instant moment;
+
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+}
